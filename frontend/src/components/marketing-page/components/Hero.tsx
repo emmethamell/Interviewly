cdf4057089e471edf00338a7ecbfb0b5846fe16d
emmet hamell <emmethamell@yahoo.com>
2025-02-04 14:42:42 -0500

    move photo to assets

--- frontend/src/components/marketing-page/components/Hero.tsx
+++ frontend/src/components/marketing-page/components/Hero.tsx
@@ -11,6 +11,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { visuallyHidden } from "@mui/utils";
 import { styled } from "@mui/material/styles";
 import { redirect } from "react-router-dom";
+import screenshotImage from "../../../assets/Interviewly_Screenshot.png";
 
 const StyledBox = styled("div")(({ theme }) => ({
   alignSelf: "center",
@@ -25,7 +26,7 @@ const StyledBox = styled("div")(({ theme }) => ({
   border: "1px solid",
   borderColor: (theme.vars || theme).palette.grey[200],
   boxShadow: "0 0 12px 8px hsla(220, 25%, 80%, 0.2)",
-  backgroundImage: `url('../../../../public/Interviewly_Screenshot.png')`,
+  backgroundImage: `url(${screenshotImage})`,
   backgroundSize: "100% 100%",
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
@@ -36,7 +37,6 @@ const StyledBox = styled("div")(({ theme }) => ({
   },
   ...theme.applyStyles("dark", {
     boxShadow: "0 0 24px 12px hsla(210, 100%, 25%, 0.2)",
-    backgroundImage: `url(${"https://mui.com"}/static/screenshots/material-ui/getting-started/templates/dashboard-dark.jpg)`,
     outlineColor: "hsla(220, 20%, 42%, 0.1)",
     borderColor: (theme.vars || theme).palette.grey[700],
   }),
