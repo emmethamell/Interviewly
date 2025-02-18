ab4ad7162a3658819febcce7729b59be3fdbcc26
emmet hamell <emmethamell@yahoo.com>
2025-02-17 23:00:53 -0500

    Remove marketing page dropdown

--- frontend/src/components/marketing-page/components/AppAppBar.tsx
+++ frontend/src/components/marketing-page/components/AppAppBar.tsx
@@ -63,7 +63,6 @@ export default function AppAppBar() {
           </Box>
           <Box
             sx={{
-              display: { xs: "none", md: "flex" },
               gap: 1,
               alignItems: "center",
             }}>
@@ -74,50 +73,7 @@ export default function AppAppBar() {
               Sign up
             </Button>
           </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" }, gap: 1 }}>
-            <IconButton aria-label="Menu button" onClick={toggleDrawer(true)}>
-              <MenuIcon />
-            </IconButton>
-            <Drawer
-              anchor="top"
-              open={open}
-              onClose={toggleDrawer(false)}
-              PaperProps={{
-                sx: {
-                  top: "var(--template-frame-height, 0px)",
-                },
-              }}>
-              <Box sx={{ p: 2, backgroundColor: "background.default" }}>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                  }}>
-                  <IconButton onClick={toggleDrawer(false)}>
-                    <CloseRoundedIcon />
-                  </IconButton>
-                </Box>
-
-                <MenuItem>Features</MenuItem>
-                <MenuItem>Testimonials</MenuItem>
-                <MenuItem>Highlights</MenuItem>
-                <MenuItem>Pricing</MenuItem>
-                <MenuItem>FAQ</MenuItem>
-                <MenuItem>Blog</MenuItem>
-                <Divider sx={{ my: 3 }} />
-                <MenuItem>
-                  <Button color="primary" variant="contained" fullWidth>
-                    Sign up
-                  </Button>
-                </MenuItem>
-                <MenuItem>
-                  <Button color="primary" variant="outlined" fullWidth>
-                    Sign in
-                  </Button>
-                </MenuItem>
-              </Box>
-            </Drawer>
-          </Box>
+      
         </StyledToolbar>
       </Container>
     </AppBar>
